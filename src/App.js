c2a5a0fe6ef20c62cbd1755fce41215f7397aa2c
Kreker990 <akparovbeka@gmail.com>
2022-06-23 23:32:09 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import Output from './components/Output/Output'
+import Search from './components/Search/Search'
+import { API } from './config'
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState()
+  const [data, setData] = useState()
+  
+  const getGiphy = async(e) => {
+    const url = API + count
+    const req = await fetch(url)
+    const res = await req.json()
+    console.log(res.data);
+    setData(res.data)
+  }
+  // useEffect(() => {
+  //   getGiphy()
+  // }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      <Search 
+      count={count}
+      setCount={setCount} 
+      getGiphy={getGiphy}
+      />
+      <Output data={data}/>
     </div>
-  );
+  )
 }
-
-export default App;
